5768299893525d5c8b3e423e8b80ea1aa1dc8736
Sakpak Donald <sakpakd@gmail.com>
2023-12-17 23:33:37 +0100

    add loader when image is loading

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -1,68 +1,43 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Text, TouchableOpacity, View, Image, ActivityIndicator, Modal } from 'react-native'
 import Swiper from 'react-native-swiper'
-import { Model } from 'react-model'
 import styles from './Carousel.styles'
 import { COLORS } from '../../constants'
 import { ChevronLeft } from '../../constants/icons'
-import { useNavigation } from '@react-navigation/native'
 
-const Slide = props => {
-  return (
-    <View style={styles.slide}>
-      <Image
-        resizeMode='contain'
-        onLoad={() => {
-          props.loadHandle(props.i)
-        }}
-        style={styles.image}
-        source={{ uri: props.uri }}
-      />
-      {!props.loaded && (
-        <View style={styles.loading}>
-            <ActivityIndicator size='large' color={COLORS.blue} />
+function Slide({uri}) {
+    const [loaded, setLoaded] = useState(false);
+    
+    return (
+        <View style={styles.slide}>
+            <Image
+            resizeMode='contain'
+            onLoad={() => {
+                setLoaded(true)
+            }}
+            style={loaded ? styles.image : {display: 'none'}}
+            source={{ uri }}
+            />
+            {
+                !loaded &&
+                <View style={styles.loading}>
+                    <ActivityIndicator size='large' color={COLORS.blue} />
+                </View>
+            }
         </View>
-      )}
-    </View>
-  )
+    )
 }
 
-const Carousel = ({ isOpen, setIsOpen, images, itemIndex }) => {
-    const SlideSchema = {
-        state: {
-            imgList: images,
-            loadQueue: []
-        },
-        actions: {
-            loaded: index => {
-                return state => {
-                    state.loadQueue[index] = 1
-                }
-            }
-        }
-    }
-    const [{ useStore }] = useState(() => Model(SlideSchema))
-    const [state, actions] = useStore()
-    const navigation = useNavigation();
-    const loadHandle = useCallback((i) => {
-        actions.loaded(i)
-    }, [])
-
+export default function Carousel({ isOpen, setIsOpen, images, itemIndex }) {
     function closeModal() {
         setIsOpen(false);
     }
-
-    useEffect(() => {
-        navigation.addListener('beforeRemove', (e) => {
-            isOpen && closeModal();
-            e.preventDefault();
-        })
-    }, [navigation])
 
     return (
         <Modal
             animationType='fade'
             visible={isOpen}
+            onRequestClose={closeModal}
         >
             <TouchableOpacity onPress={closeModal} activeOpacity={0.8} style={styles.returnBtn}>
                 <View style={styles.returnView}>
@@ -73,25 +48,17 @@ const Carousel = ({ isOpen, setIsOpen, images, itemIndex }) => {
                 </View>
             </TouchableOpacity>
             <Swiper
-                showsPagination = {false}
-                loadMinimal
-                loadMinimalSize={1}
                 loop={false}
                 style={styles.wrapper}
                 index={itemIndex}
+                showsPagination={false}
             >
-                {state.imgList.map((item, i) => (
-                    <Slide
-                    loadHandle={loadHandle}
-                    uri={item}
-                    i={i}
-                    key={i}
-                    loaded={state.loadQueue[i]}
-                    />
-                ))}
+                {
+                    images.map((uri, key) => (
+                        <Slide key={key} uri={uri} />
+                    ))
+                }
             </Swiper>
         </Modal>
     )
 }
-
-export default Carousel
