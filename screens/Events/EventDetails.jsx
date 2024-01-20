702dc70b7e8a504623a327fadabb5da37695317d
Sakpak Donald <sakpakd@gmail.com>
2024-01-20 23:34:42 +0100

    update event page and add map

--- screens/Events/EventDetails.jsx
+++ screens/Events/EventDetails.jsx
@@ -1,54 +1,56 @@
-import { View, Text, ScrollView, ImageBackground, StyleSheet, Image, SafeAreaView } from 'react-native'
+import { View, Text, ScrollView, ImageBackground, StyleSheet, SafeAreaView } from 'react-native'
 import React from 'react'
 import { COLORS, FONT, PADDINGS, SIZES } from '../../constants'
 import { useRoute } from '@react-navigation/native';
 import Header from '../../components/Header/Header';
 import BackBtn from '../../components/BackBtn/BackBtn';
-import { CalendarIcon, LocationIcon } from '../../constants/icons';
-
-function EventData({location}) {
-    return (
-        <View style={styles.eventDataContainer}>
-            <View style={styles.eventData}>
-                <View style={styles.eventIconContainer}>
-                    <LocationIcon height={22} width={22} fill={COLORS.blue} />
-                </View>
-                <View style={styles.eventDataText}>
-                    <Text style={styles.eventDataHeading}>{location}</Text>
-                    <Text style={styles.eventDataSubHeading}>Lieu</Text>
-                </View>
-            </View>
-            <View style={styles.eventData}>
-                <View style={styles.eventIconContainer}>
-                    <CalendarIcon height={22} width={22} stroke={COLORS.blue} />
-                </View>
-                <View style={styles.eventDataText}>
-                    <Text style={styles.eventDataHeading}>24, Nov 2024</Text>
-                </View>
-            </View>
-        </View>
-    )
-}
+import Overlay from '../../components/Overlay/Overlay';
+import MapView from 'react-native-maps';
 
 export default function EventDetails() {
     const { image, location, title, content } = useRoute().params;
-  return (
-    <SafeAreaView style={styles.frame}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={styles.container}>
-                <View style={styles.imgBg}>
-                    <Header iconColor={COLORS.white} LeftIcon={BackBtn} btnStyle={{backgroundColor: "rgba(0, 0, 0, 0.3)"}} viewStyle={{padding: PADDINGS.page, zIndex: 2}} />
-                    <Image style={styles.img} source={{ uri: image }} />
+
+    return (
+        <SafeAreaView style={styles.frame}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.container}>
+                    <ImageBackground style={styles.imgBg} source={{ uri: image }}>
+                        <Overlay />
+                        <Header LeftIcon={BackBtn} iconColor={COLORS.white} viewStyle={{padding: PADDINGS.page, zIndex: 2}} />
+                        <View style={styles.headingContainer}>
+                            <Text style={styles.location}>{location}</Text>
+                            <Text style={styles.title}>{title}</Text>
+                        </View>
+                    </ImageBackground>
+                    <View style={styles.details}>
+                        <View style={styles.detailContainer}>
+                            <Text style={styles.detailHeading}>Date</Text>
+                            <Text style={styles.content}>Sam, 22 Janvier 2024</Text>
+                        </View>
+                        <View style={styles.detailContainer}>
+                            <Text style={styles.detailHeading}>Details</Text>
+                            <Text style={styles.content}>{content}</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.detailHeading}>Lieu</Text>
+                            <Text style={styles.content}>{location}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.mapContainer}>
+                        <MapView
+                            style={styles.map}
+                            initialRegion={{
+                                latitude: 37.78825,
+                                longitude: -122.4324,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        />
+                    </View>
                 </View>
-                <EventData location={location} />
-                <View style={styles.detailContainer}>
-                    <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.content}>{content}</Text>
-                </View>
-            </View>
-        </ScrollView>
-    </SafeAreaView>
-  )
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -56,65 +58,50 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     container: {
-        padding: PADDINGS.page/2,
         display: "flex",
-        gap: PADDINGS.element,
+        gap: PADDINGS.gapMd,
+        paddingBottom: 20,
     },
     imgBg: {
         height: SIZES.ScreenHeight*0.45,
         width: SIZES.full,
+        backgroundColor: "red"
     },
-    img: {
+    headingContainer: {
         position: "absolute",
-        top: 0,
-        left: 0,
-        height: SIZES.full,
-        width: SIZES.full,
-        borderRadius: 20,
+        bottom: PADDINGS.page,
+        left: PADDINGS.page
     },
-    eventDataContainer: {
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        gap: PADDINGS.gap
-    },
-    eventData: {
-        display: "flex",
-        flexDirection: "row",
-        gap: PADDINGS.gap,
-        paddingHorizontal: PADDINGS.page,
-    },
-    eventIconContainer: {
-        padding: 10,
-        backgroundColor: COLORS.white,
-        borderRadius: 10
-    },
-    eventDataText: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        gap: 2,
-    },
-    eventDataHeading: {
+    location: {
+        color: COLORS.lightGray,
         fontFamily: FONT.articleMedium,
         fontSize: SIZES.sm,
     },
-    eventDataSubHeading: {
-        fontFamily: FONT.articleMedium,
-        fontSize: SIZES.xs-1,
-        color: COLORS.gray,
-    },
-    detailContainer: {
-        paddingHorizontal: PADDINGS.page/2,
-    },
     title: {
+        color: COLORS.white,
         fontFamily: FONT.articleBold,
-        fontSize: SIZES.lg,
-        textTransform: "capitalize"
+        fontSize: SIZES.xxl,
+        width: (SIZES.ScreenWidth*0.75) - PADDINGS.page*2 ,
+    },
+    details: {
+        paddingHorizontal: PADDINGS.page,
+        display: "flex",
+        gap: PADDINGS.gapMd,
+    },
+    detailHeading: {
+        fontFamily: FONT.articleSemiBold,
+        fontSize: SIZES.sm,
     },
     content: {
-        marginTop: 10,
         fontFamily: FONT.articleRegular,
-        fontSize: SIZES.xs
+        fontSize: SIZES.xs,
+        color: "rgba(0, 0, 0, 0.7)"
+    },
+    mapContainer: {
+        paddingHorizontal: PADDINGS.page
+    },
+    map: {
+        height: SIZES.ScreenHeight*0.4,
+        width: SIZES.full,
     }
 })
